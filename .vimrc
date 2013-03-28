3f6d04c540f7058ed6cd01de4f45873d079ac912
Andy LaVoy <lavoy.andy@gmail.com>
2013-03-28 11:18:23 -0700

    moved to Solarized Dark Theme

--- .vimrc
+++ .vimrc
@@ -1,8 +1,9 @@
-syntax on
+syntax enable
+set background=dark
+colorscheme solarized
 
 au BufNewFile,BufRead *.rabl set filetype=ruby
 
 au BufRead,BufNewFile *.json set filetype=json
 au! Syntax json source /Users/andy/.vim/ftplugin/json.vim
 
-:colorscheme Tomorrow-Night-Bright
--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 git pull
 function doIt() {
-  rsync --exclude ".git/" --exclude ".gitignore" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" -av . ~
+  rsync --exclude ".git/" --exclude ".gitignore" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" --exclude "solarized-dark/" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
   doIt
